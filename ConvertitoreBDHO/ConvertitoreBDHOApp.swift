44ffde16b1f5145561d40058fd2b1ed40aafefb1
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-25 12:33:27 +0200

    Initial Commit

---
+++ ConvertitoreBDHO/ConvertitoreBDHOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertitoreBDHOApp.swift
+//  ConvertitoreBDHO
+//
+//  Created by Michele Manniello on 25/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertitoreBDHOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
